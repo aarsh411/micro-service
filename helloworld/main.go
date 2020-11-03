782cccdd764aaf55c79fb22a2643b232e714b15f
Aarsh Makwana <59681126+aarsh411@users.noreply.github.com>
2020-11-03 15:13:55 +0530

    Add files via upload

--- helloworld/main.go
+++ helloworld/main.go
@@ -1,10 +1,14 @@
 package main
 
 import (
-	"C:/Go/helloworld/handlers"
+	"context"
 	"log"
 	"net/http"
 	"os"
+	"time"
+	"time/signal"
+
+	"github.com/aarsh411/micro-service/helloworld/handlers"
 )
 
 func main() {
@@ -14,5 +18,25 @@ func main() {
 	sm := http.NewServeMux()
 	sm.Handle("/", hh)
 	sm.Handle("/Goodbye", gg)
-	http.ListenAndServe(":9900", nil)
+	s := &http.Server{
+		Addr:         ":9900",
+		Handler:      sm,
+		IdleTimeout:  120 * time.Second,
+		ReadTimeout:  1 * time.Second,
+		WriteTimeout: 1 * time.Second,
+	}
+	gofunc(){
+		err:= s.ListenAndServe()
+		if err != nil{
+			l.Fatal(error)
+		}
+	}()
+	sigChan := make(chan os.Signal)
+	signal.Notify(sigChan, os.Interrupt)
+	signal.Notify(sigChan, os.Kill)
+	sig := <- sigChan
+	l.Println("received terminate",sig)
+
+	tc, _ := context.WithTimeout(context.Background(), 30*time.Second)
+	s.Shutdown(tc)
 }
